59a4f039f4bf242e9f8fedc220a86c2cf60d1925
Bakbergen-Samatov <bakbergensamato04@gmail.com>
2020-05-22 21:57:15 +0600

    tt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import './App.css';
-import Layout from './containers/Layout/Layout';
-import CakeBuilder from './containers/CakeBuilder/CakeBuilder';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./containers/Layout/Layout";
+import CakeBuilder from "./containers/CakeBuilder/CakeBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
+import "./App.css";
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-        <CakeBuilder />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/builder" />
+          </Route>
+          <Route path="/builder">
+            <CakeBuilder />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
 };
+
