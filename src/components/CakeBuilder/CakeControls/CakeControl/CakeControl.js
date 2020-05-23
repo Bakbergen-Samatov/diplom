dd03c66e3532e4e0dd9c2bae2c6afbc2b07682ab
Bakbergen-Samatov <bakbergensamato04@gmail.com>
2020-05-23 23:36:56 +0600

    ...

--- src/components/CakeBuilder/CakeControls/CakeControl/CakeControl.js
+++ src/components/CakeBuilder/CakeControls/CakeControl/CakeControl.js
@@ -1,21 +1,31 @@
 import React from "react";
 import classes from "./CakeControl.module.css";
+import { useDispatch } from "react-redux";
+import { ADD_INGREDIENT, REMOVE_INGREDIENT } from "../../../../store/actions";
 
-export default ({ control, removeIngredient, addIngredient, disabled }) => (
-  <div className={classes.CakeControl}>
-    <button
-      className={classes.less}
-      onClick={() => removeIngredient(control.type)}
-      disabled={disabled}
-    >
-      -
-    </button>
-    <span className={classes.label}>{control.label}</span>
-    <button
-      className={classes.more}
-      onClick={() => addIngredient(control.type)}
-    >
-      +
-    </button>
-  </div>
-);
+export default ({ control, removeIngredient, addIngredient, disabled }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.SushiControl}>
+      <button
+        className={classes.less}
+        onClick={() =>
+          dispatch({ type: REMOVE_INGREDIENT, ingredient: control.type })
+        }
+        disabled={disabled}
+      >
+        -
+      </button>
+      <span className={classes.label}>{control.label}</span>
+      <button
+        className={classes.more}
+        onClick={() =>
+          dispatch({ type: ADD_INGREDIENT, ingredient: control.type })
+        }
+      >
+        +
+      </button>
+    </div>
+  );
+};
